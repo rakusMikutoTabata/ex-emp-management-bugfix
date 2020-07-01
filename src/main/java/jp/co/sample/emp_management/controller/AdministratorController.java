737c4934192dc20a8840905fbfa5f121e488d9f9
rakusMikutoTabata <mikuto.tabata@rakus.co.jp>
2020-07-01 14:34:03 +0900

    (2-1)ログイン後、従業員一覧と従業員詳細画面に「〇〇さん こんにちは！」と表示するように変更

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -123,6 +123,7 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		model.addAttribute("administratorName",administrator.getName());
 		return "forward:/employee/showList";
 	}
 

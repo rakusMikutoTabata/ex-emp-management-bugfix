9fba85bea2d5a55098664844246fe21c8061922d
rakusMikutoTabata <mikuto.tabata@rakus.co.jp>
2020-07-03 10:13:44 +0900

    (6-5)従業員一覧を1ページに10件までの表示にするよう変更

---
+++ src/main/java/jp/co/sample/emp_management/repository/EmployeePageRepository.java
@@ -0,0 +1,5 @@
+package jp.co.sample.emp_management.repository;
+
+public class EmployeePageRepository {
+
+}
---
+++ src/main/resources/static/js/paging.js
@@ -0,0 +1,21 @@
+$(function() {
+  var page = 0;
+  function draw() {
+    $('#page').html('ページ'+(page + 1));
+    $('tr').hide();
+    $('tr:first,tr:gt(' + page * 10 + '):lt(10)').show();
+  }
+  $('#prev').click(function() {
+    if (page > 0) {
+      page--;
+      draw();
+    }
+  });
+  $('#next').click(function() {
+    if (page < ($('tr').size() - 1) / 10 - 1) {
+      page++;
+      draw();
+    }
+  });
+  draw();
+});
